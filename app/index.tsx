064651f94a8979eb85fba13461bb72c81e7890f3
Emi Legaspi <emi@wipster.io>
2025-01-27 09:07:17 +1300

    Add a text input

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,48 @@
-import { StyleSheet, View } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
 import { ShoppingListItem } from '../components/ShoppingListItem'
 import { theme } from '../theme'
 
+type ShoppingListItemType = {
+  id: string
+  name: string
+}
+
+const initialList: ShoppingListItemType[] = [
+  { id: '1', name: 'Coffee' },
+  { id: '2', name: 'Tea' },
+  { id: '3', name: 'Sugar' },
+]
+
 export default function App() {
+  const [value, setValue] = useState('')
+  const [shoppingList, setShoppingList] = useState(initialList)
+
+  const handleSubmit = () => {
+    if (value) {
+      setShoppingList([
+        { id: new Date().toTimeString(), name: value },
+        ...shoppingList,
+      ])
+      setValue('')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Sugar" isCompleted />
+      <TextInput
+        placeholder="e.g. Coffee"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+        autoCorrect={false}
+      />
+
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
     </View>
   )
 }
@@ -16,6 +51,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.white,
-    justifyContent: 'center',
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colors.lightGrey,
+    borderWidth: 2,
+    borderRadius: 50,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
   },
 })
