0e813c4b6a0bf76e62d2cb1d75a6f68dd49da38b
Emi Legaspi <emi@wipster.io>
2025-01-27 12:14:25 +1300

    Updates to the countdown timer

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -3,6 +3,7 @@ import * as Device from 'expo-device'
 import * as Notifications from 'expo-notifications'
 import { useEffect, useState } from 'react'
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { TimeSegment } from '../../components/TimeSegment'
 import { theme } from '../../theme'
 import { registerForPushNotificationsAsync } from '../../utils/notification'
 import { CountdownStatus } from '../../utils/types'
@@ -59,8 +60,43 @@ export default function CounterScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>{secondsElapsed}</Text>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.overdueContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.heading,
+          status.isOverdue ? styles.overdueText : undefined,
+        ]}
+      >
+        {status.isOverdue ? 'Thing overdue by...' : 'Thing due in...'}
+      </Text>
+
+      <View style={styles.row}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance.days ?? 0}
+          textStyle={status.isOverdue ? styles.overdueText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance.hours ?? 0}
+          textStyle={status.isOverdue ? styles.overdueText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.overdueText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.overdueText : undefined}
+        />
+      </View>
 
       <TouchableOpacity
         style={styles.button}
@@ -80,6 +116,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: theme.colors.white,
   },
+  overdueContainer: {
+    backgroundColor: theme.colors.red,
+  },
   button: {
     backgroundColor: theme.colors.black,
     borderRadius: 6,
@@ -90,5 +129,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textTransform: 'uppercase',
     letterSpacing: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginBottom: 24,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  overdueText: {
+    color: theme.colors.white,
   },
 })
