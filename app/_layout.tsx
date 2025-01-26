cb978574f14bb3eb6f38937f408e4c93ea9cc4b7
Emi Legaspi <emi@wipster.io>
2025-01-27 08:10:16 +1300

    Set up bottom tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,46 @@
-import { Stack } from 'expo-router'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import { Tabs } from 'expo-router'
+import { theme } from '../theme'
 
 const Layout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Shopping List' }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colors.cerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Shopping List',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: 'Counter',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons
+              name="access-time-filled"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: 'Idea',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   )
 }
 
---
+++ app/counter.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+import { theme } from '../theme'
+
+export default function CounterScreen() {
+  const router = useRouter()
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Counter</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.white,
+  },
+  text: {
+    fontSize: 24,
+  },
+})
